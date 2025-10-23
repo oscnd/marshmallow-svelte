99f5b7e21e99660d828efb0032c2a39c473fab4e
Thun (MacBook Pro) <bsthun@gmail.com>
2025-10-23 18:24:54 +0700

    Initialize storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
-import {defineConfig} from 'vite'
-import {svelte} from '@sveltejs/vite-plugin-svelte'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-    plugins: [
-        svelte(),
-        tailwindcss(),
-    ],
+	plugins: [svelte(), tailwindcss()],
+	test: {
+		projects: [
+			{
+				extends: true,
+				plugins: [
+					// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+					storybookTest({
+						configDir: path.join(dirname, '.storybook'),
+					}),
+				],
+				test: {
+					name: 'storybook',
+					browser: {
+						enabled: true,
+						headless: true,
+						provider: 'playwright',
+						instances: [
+							{
+								browser: 'chromium',
+							},
+						],
+					},
+					setupFiles: ['.storybook/vitest.setup.ts'],
+				},
+			},
+		],
+	},
 })
