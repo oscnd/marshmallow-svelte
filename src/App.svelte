c3f8ac45e8c03e68efa97ab17a41540ca85e52ee
Thun (MacBook Pro) <bsthun@gmail.com>
2025-10-23 18:14:50 +0700

    Initialize tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-	import Counter from './lib/Counter.svelte'
+    import Counter from './lib/Counter.svelte'
+    import './style/style.scss'
+    import './style/tailwind.css'
 </script>
 
 <main>
-	<h1>Vite + Svelte</h1>
+    <h1>Vite + Svelte</h1>
 
-	<div class="card">
-		<Counter />
-	</div>
+    <div class="card p-2">
+        <Counter/>
+    </div>
 
-	<p>
-		Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the
-		official Svelte app framework powered by Vite!
-	</p>
+    <p>
+        Check out <a href="https://github.com/sveltejs/kit#readme" rel="noreferrer" target="_blank">SvelteKit</a>, the
+        official Svelte app framework powered by Vite!
+    </p>
 
-	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+    <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
