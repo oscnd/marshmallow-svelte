9309ffd870f45cfca8f90fe6ea22298fc358d8d4
Thun (MacBook Pro) <bsthun@gmail.com>
2025-10-25 13:22:36 +0700

    Add component export

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Marshmallow Svelte: Main Library Export
+
+import '$/style/tailwind.css'
+export * from './component'
+
+export { cn } from './lib/utils'
+export type { Palette } from './declaration/palette'
